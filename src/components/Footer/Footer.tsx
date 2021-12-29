e9722a9e8ca66bfda8d98f6dcd1cb0ecb3f44e8a
Erik Rehn <erik.rehn@altostruct.se>
2021-12-29 02:03:44 +0100

    small change to the footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,25 +4,28 @@ import "./Footer.scss";
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>Copyright &copy; 2021 Altostruct AB</p>
-      <p>
-        Kontakt: <a href="mailto:info@altostruct.se">info@altostruct.se</a>
-      </p>
-      <p>Org nr: 559244-1561</p>
-      <p>Stockholms län, Lidingö</p>
-      <p>
-        Icons made by{" "}
-        <a
-          href="https://www.flaticon.com/authors/dreamstale"
-          title="Dreamstale"
-        >
-          Dreamstale
-        </a>{" "}
-        from{" "}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>
-      </p>
+      <div>
+        <p>Copyright &copy; 2021 Altostruct AB</p>
+        <p>
+          Kontakt: <a href="mailto:info@altostruct.se">info@altostruct.se</a>
+        </p>
+        <p>Org nr: 559244-1561</p>
+        <p>Address: Tegelbacken 4A, 111 52 Stockholm</p>
+        <p>
+          Icons made by{" "}
+          <a
+            href="https://www.flaticon.com/authors/dreamstale"
+            title="Dreamstale"
+          >
+            Dreamstale
+          </a>{" "}
+          from{" "}
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </p>
+      </div>
+      <div></div>
     </footer>
   );
 };
