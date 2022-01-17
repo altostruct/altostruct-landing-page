dfc219039e3e0dc6e9bda2b49b23f02f665a7f91
Rasmus Holmgren <ras.holmgren@gmail.com>
2022-01-17 22:00:48 +0100

    worked on about us page, added aboutus.scss

--- src/pages/aboutus.tsx
+++ src/pages/aboutus.tsx
@@ -1,9 +1,66 @@
 import * as React from "react";
+import Footer from "@components/Footer/Footer";
+import Topbar from "@components/Topbar/Topbar";
+import Icon from "@components/Icon";
+import "@styles/global.scss";
+import "./aboutus.scss";
+
 function AboutUs() {
-    const [count, setCount] = React.useState(0);
-    function onClick(){
-        setCount(count + 1)
-    }
-  return <div><button style={{backgroundColor:"red"}} onClick={onClick}>clickme</button><h1>Hej Rasmus {count}</h1></div>;
+  const team = [
+    {
+      name: "Rasmus Holmgren",
+      title: "Fullstack developer",
+      presentation: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
+        Aenean commodo ligula eget dolor. Aenean massa. 
+        Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+        Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.`,
+      mail: "rasmus.holmgren@altostruct.se",
+      id: 1,
+    },
+    {
+      name: "Rasmus Holmgren",
+      title: "Fullstack developer",
+      presentation: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
+        Aenean commodo ligula eget dolor. Aenean massa. 
+        Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+        Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.`,
+      mail: "rasmus.holmgren@altostruct.se",
+      id: 1,
+    },
+  ];
+
+  return (
+    <main className="page">
+      <Topbar></Topbar>
+      <div className="title content screen-height">
+        <Icon.Square className="top-image" />
+        <div className="aboutus-content">
+          <div className="sm-screen-height">
+            <label>Om vårt</label>
+            <h1 className="primary">Team</h1>
+          </div>
+
+          {team.map((teamMember) => (
+            <div className="split-members" key={teamMember.id}>
+              <h2>{teamMember.name}</h2>
+              <label>{teamMember.title}</label>
+              <p>
+                <span className="aboutus-presentation">
+                  {teamMember.presentation}
+                </span>
+              </p>
+              <a
+                className="aboutus-mail primary"
+                href={"mailto:" + teamMember.mail}
+              >
+                {teamMember.mail}
+              </a>
+            </div>
+          ))}
+        </div>
+      </div>
+      <Footer></Footer>
+    </main>
+  );
 }
 export default AboutUs;
