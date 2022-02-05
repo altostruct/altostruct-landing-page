a61cd3a58f15a05175dc178bd315140192e70dcd
Rasmus Holmgren <ras.holmgren@gmail.com>
2022-02-05 17:15:52 +0100

    simplified setLanguage()

--- src/hooks/useTranslation.ts
+++ src/hooks/useTranslation.ts
@@ -18,102 +18,73 @@ const DEFAULT_LANGUAGE = "swe";
  * @returns a translated string
  */
 const useTranslation = () => {
-  const isBrowser = typeof window !== "undefined"
- 
+  const isBrowser = typeof window !== "undefined";
+
   //default fallback
   let language = DEFAULT_LANGUAGE;
-  let currentPath : string;
-  
-  //get current language from URL
-  if(isBrowser){
-  currentPath = window.location.pathname
-  console.log("Current path:" + currentPath)//debug
-  for (const lang of LANGUAGES) {
-     if(currentPath.startsWith("/" + lang) || (currentPath == lang)){
-      language = lang;
-     }
-     //used for testing on github domain
-     if(currentPath.startsWith("/altostruct-landing-page/" + lang)){
-      language = lang;
-     }
-   }
-  }
-  console.log("Current language:" + language);//debug
- 
+  let currentPath: string;
 
-  // TODO
-  // 1. read in file from locales folder
-  // 2. Figure which language we are current using
-  //    by looking at the url.
-  // 3. a)
-  //    replace the text based on the locales folder.
-  //    for example if text = "Hej" and and it exists
-  //    ...
-  //    "Hej": "Hello"
-  //    ...
-  //    in the file we want to replace "Hej" with "Hello"
-  //    b)
-  //    If the text does not exists in the translations folder
-  //    output a warning to the log and the use the text. For
-  //    example text = "Hej" and no translation exists then just
-  //    output "Hej" again
-  // 4. If the text containes "replacement values" replace the
-  //    value with the varible.
+  //get current language from URL
+  if (isBrowser) {
+    currentPath = window.location.pathname;
+    console.log("Current path:" + currentPath); //debug
+    for (const lang of LANGUAGES) {
+      if (currentPath.startsWith("/" + lang) || currentPath == lang) {
+        language = lang;
+      }
+      //used for testing on github domain
+      if (currentPath.startsWith("/altostruct-landing-page/" + lang)) {
+        language = lang;
+      }
+    }
+  }
+  console.log("Current language:" + language); //debug
 
   /**Returns a translated string with values of any existing variable.*/
   const t = (text: string, variables?: Record<string, any>): string => {
-    const translations = require("@locales/" + language + "/translation.json");
-   
-    if(!translations.hasOwnProperty(text)){
+    const translations = zrequire("@locales/" + language + "/translation.json");
+
+    if (!translations.hasOwnProperty(text)) {
       console.warn("No translation for: '" + text + "'");
-    }
-    else{
+    } else {
       text = translations[text];
     }
 
-    if(variables){
+    if (variables) {
       // replace each variable with its value
-      for(const variableKey in variables){
+      for (const variableKey in variables) {
         let regex = new RegExp(`{{${variableKey}}}`, "g");
         text = text.replace(regex, variables[variableKey]);
       }
     }
-    return text
+    return text;
   };
 
   /**Sets the URL to the correct language.*/
   const setLanguage = (language: string) => {
-    if(!isBrowser){
+    if (!isBrowser) {
       return;
     }
-    let currentPathSplit = currentPath.split("/");
+    //remove the first element, allways starts with a dash
+    let currentPathSplit = currentPath.split("/").slice(1);
     //fallback if set fails
     let newPath = "/" + DEFAULT_LANGUAGE;
-    
-    let isLangSet = false;
-    
-    for (const lang of LANGUAGES) {
-    //when a language is set
-    if(currentPath.includes(lang)){
-      //replace current language with new
-      currentPathSplit[currentPathSplit.indexOf(lang)] = language;
-      newPath = currentPathSplit.join("/");
-      console.log("New path:" + newPath) //debug
-      window.location.pathname = newPath;
-      isLangSet = true;
-      break;
+
+    const isLanguageSet = LANGUAGES.includes(currentPathSplit[0]);
+    const languagePrefix = language === DEFAULT_LANGUAGE ? null : language;
+
+    if (isLanguageSet) {
+      window.location.pathname = [languagePrefix, ...currentPathSplit.slice(1)]
+        .filter((subPath) => subPath !== null)
+        .join("/");
+    } else {
+      window.location.pathname = [languagePrefix, ...currentPathSplit]
+        .filter((subPath) => subPath !== null)
+        .join("/");
     }
-  }
+  };
   //when no language is set
-  if(!isLangSet){
-      //insert language in the URL
-      currentPathSplit.splice(1, 0, language);
-      newPath = currentPathSplit.join("/");
-      window.location.pathname = newPath;
-    } 
-}
   return { t, language: language, setLanguage: setLanguage };
 };
 
 export default useTranslation;
-
