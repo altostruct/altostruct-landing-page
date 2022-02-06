2dbe5aa6074dc7628de885366d9aa4c70904a606
rajtarn <90709334+rajtarn@users.noreply.github.com>
2022-02-06 19:31:01 +0000

    Deploying to gh-pages from @ erikschmutz/altostruct-landing-page@1db48b35d4abf2d05d9da03c1fee7acaa10c8cd8 🚀

--- component---src-pages-aboutus-tsx-fbd28481df4ba3ca4a8d.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkaltostruct_website_gatsby=self.webpackChunkaltostruct_website_gatsby||[]).push([[468],{5594:function(e,t,n){n.r(t);var u=n(7294);t.default=function(){var e=u.useState(0),t=e[0],n=e[1];return u.createElement("div",null,u.createElement("button",{style:{backgroundColor:"red"},onClick:function(){n(t+1)}},"clickme"),u.createElement("h1",null,"Hej Rasmus ",t))}}}]);
-//# sourceMappingURL=component---src-pages-aboutus-tsx-fbd28481df4ba3ca4a8d.js.map
