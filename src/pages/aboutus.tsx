1db48b35d4abf2d05d9da03c1fee7acaa10c8cd8
Rasmus Holmgren <ras.holmgren@gmail.com>
2022-02-06 20:28:12 +0100

    removed aboutus test page from prod

--- src/pages/aboutus.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-function AboutUs() {
-    const [count, setCount] = React.useState(0);
-    function onClick(){
-        setCount(count + 1)
-    }
-  return <div><button style={{backgroundColor:"red"}} onClick={onClick}>clickme</button><h1>Hej Rasmus {count}</h1></div>;
-}
-export default AboutUs;
