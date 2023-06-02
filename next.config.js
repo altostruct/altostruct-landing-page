ed562f10056e877cfd4179ef2e11b217b1709ae7
William Redgård <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>
2023-06-02 18:06:57 +0200

    language error fix

--- next.config.js
+++ next.config.js
@@ -50,7 +50,7 @@ const nextConfig = {
   i18n: {
     // These are all the locales you want to support in
     // your application
-    locales: ["sv"],
+    locales: ["en-US", "sv"],
     // This is the default locale you want to be used when visiting
     // a non-locale prefixed path e.g. `/hello`
     defaultLocale: "sv",
