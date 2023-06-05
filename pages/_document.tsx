271c2a0e13503f3fb8f3bf7af3dcb09de2511961
Erik Rehn <erik.rehn@altostruct.se>
2023-06-05 16:25:31 +0200

    -

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,18 +7,7 @@ export default class MyDocument extends Document {
       <Html>
         <Head></Head>
         <body>
-          <Script
-            src="https://www.googletagmanager.com/gtag/js?id=G-RS6NEMYS3W"
-            strategy="afterInteractive"
-          />
-          <Script id="google-analytics" strategy="afterInteractive">
-            {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-RS6NEMYS3W');
-        `}
-          </Script>
+
           <Main />
           <NextScript />
         </body>
