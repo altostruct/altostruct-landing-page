b8dd4e932a6c727e8e1688894787670f9c96f606
William Redgård <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>
2023-06-13 14:51:09 +0200

    google ads tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,6 +18,20 @@ export default function App({ Component, pageProps }: AppProps) {
           gtag('config', 'G-RS6NEMYS3W');
         `}
       </Script>
+
+    {/* google ads */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-6T55W0J1S2"
+        strategy="afterInteractive"
+      />
+      <Script id="google-ads" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-6T55W0J1S2');
+        `}
+      </Script>
       <Component {...pageProps} />
     </>
   );
