9e148c769c9097cb63ce3801ae700f3be479d806
Mustafa Ali <mustaafaa.aali@gmail.com>
2023-09-07 15:36:01 +0200

    small fixes

--- components/ReferenceCases.tsx
+++ components/ReferenceCases.tsx
@@ -14,24 +14,24 @@ interface ReferenceCasesProps {
 }
 
 function ReferenceCases(props: ReferenceCasesProps) {
-    const { data, title = "Case Studies.", start, end } = props
+    const { data, title = "Case Studies", start, end } = props
 
     return <Card title={title}>
-        <div className="flex flex-col md:h-96 md:flex-row gap-24 md:gap-3 mt-4 divide-gray-800 divide-2">
+        <div className="flex flex-col md:h-max md:flex-row gap-24 md:gap-3 mt-4 divide-gray-800 divide-2">
             {data.slice(start, end).map((item: any, index: number) => {
                 return <div className="md:w-1/3  text-gray-300 flex flex-col md:first:pl-0 md:px-6 md:last:pr-0" key={index}>
                     <div className="flex justify-between">
-                        <div className="mb-3">
-                            <p className="text-2xl md:text-3xl mb-3">{item.fields.shortDescriptionTitle}</p>
-                            <div className="text-xl flex gap-1">
-                                {item.fields.tags?.slice(0, 5).map((tag: string, index: number) => {
-                                    return <WordCircled key={index} borderCircle>{tag}</WordCircled>
-                                })}
-                            </div>
+                        <div className="mb-3 font-black">
+                            <p className="text-2xl md:text-2xl mb-3">{item.fields.shortDescriptionTitle}</p>
                         </div>
                     </div>
-                    <p className="text-xl text-ellipsis overflow-hidden flex-grow">{item.fields.shortDescription} </p>
+                    <p className="text-lg text-ellipsis overflow-hidden flex-grow">{item.fields.shortDescription} </p>
                     <Button type="secondary" link={"/cases/" + item.fields.slug} className="mt-4 ml-auto align-bottom">Läs mer</Button>
+                    <div className="text-xl flex gap-1 md:pt-5 pt-5">
+                        {item.fields.tags?.slice(0, 5).map((tag: string, index: number) => {
+                            return <WordCircled key={index} borderCircle>{tag}</WordCircled>
+                        })}
+                    </div>
                 </div>
             })}
         </div>
