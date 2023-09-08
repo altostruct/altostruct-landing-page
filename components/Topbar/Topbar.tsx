90afea253897eb2f4382de1be6ea6f9a4dee20b5
Mustafa Ali <mustaafaa.aali@gmail.com>
2023-09-08 12:39:12 +0200

    fixed build

--- components/Topbar/Topbar.tsx
+++ components/Topbar/Topbar.tsx
@@ -87,8 +87,8 @@ const Topbar = (props: { transparent?: boolean; fixed?: boolean }) => {
 
           <Dropdown icon={<svg className="rotate-180" width="8px" height="8px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="none"><path fill="#c3eec3" d="M8 1.25a2.101 2.101 0 00-1.785.996l.64.392-.642-.388-5.675 9.373-.006.01a2.065 2.065 0 00.751 2.832c.314.183.67.281 1.034.285h11.366a2.101 2.101 0 001.791-1.045 2.064 2.064 0 00-.006-2.072L9.788 2.25l-.003-.004A2.084 2.084 0 008 1.25z" /></svg>}
           title="Tjänster" className={classNames(styles["button-spacing"], "text-lg")}>
-          {getContentfulProducts().map(v=>{
-            return <div className="flex">
+          {getContentfulProducts().map((v,index)=>{
+            return <div className="flex" key={index}>
             <svg className="rotate-90 mt-[7px] mr-2" width="8px" height="8px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="none"><path fill="#c3eec3" d="M8 1.25a2.101 2.101 0 00-1.785.996l.64.392-.642-.388-5.675 9.373-.006.01a2.065 2.065 0 00.751 2.832c.314.183.67.281 1.034.285h11.366a2.101 2.101 0 001.791-1.045 2.064 2.064 0 00-.006-2.072L9.788 2.25l-.003-.004A2.084 2.084 0 008 1.25z" /></svg>
             <Link
               className={classNames("group")}
