ae87edb8703dc6576eb395ec684662e05efbcf55
Emilio Gustavsson <emilio.gustavsson@hotmail.com>
2024-01-04 10:48:55 +0100

    weird error do not now

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react"
 
-const Button = (props: { label: string, onClick: (event: React.MouseEvent<HTMLAnchorElement>) => void }) => {
+const Button = (props: { label: string, onClick: (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void }) => {
     const { label, onClick } = props
     return <a onClick={onClick} className="p-2 border-2 border-black cursor-pointer hover:border-slate-900 w-32 text-center ">{label} </a>
 }
