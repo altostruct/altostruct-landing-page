db33f384451d2e86683f8dd4647507e8c362b45b
Emilio Gustavsson <emilio.gustavsson@hotmail.com>
2024-01-04 11:24:23 +0100

    -

--- components/Contentful/index.tsx
+++ components/Contentful/index.tsx
@@ -15,7 +15,6 @@ export function ContentfulImage(
 
   return (
     // eslint-disable-next-line jsx-a11y/alt-text
-    console.log("images/contentful/" + image.sys.id + "." + ext),
     <Image
       height={
         height ||
@@ -27,7 +26,7 @@ export function ContentfulImage(
         image.fields.details?.image.width ||
         image.fields?.file.details.image.width
       }
-      src={"images/contentful/" + image.sys.id + "." + ext}
+      src={process.env.GITHUB_PAGE_ROOT_URL ?? "/" + "images/contentful/" + image.sys.id + "." + ext}
       className="w-full"
       style={{
         objectFit: "cover",
