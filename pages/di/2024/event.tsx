1e7b26cd711197c771cb0a704d1b7eb6f897f902
Erik Rehn <erik.rehn@altostruct.se>
2024-06-24 17:49:21 +0200

    added GA to the DI tracker

--- pages/di/2024/event.tsx
+++ pages/di/2024/event.tsx
@@ -3,12 +3,14 @@ import Topbar from "@components/Topbar/Topbar"
 import { useRouter } from "next/router"
 import { Content } from "pages"
 import { useEffect } from "react"
+import { sendGAEvent } from '@next/third-parties/google'
 
 function DIPage() {
     const router = useRouter();
     
     useEffect(()=>{
         (async () => {
+            sendGAEvent("Events", "DI-QR-SCANNED");
             router.replace("/events/2024/cloud-and-ai-study-2024")
         })()
     }, [router]);
